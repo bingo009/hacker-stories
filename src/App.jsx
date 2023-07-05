0c6a5d26ab35a49bb589f170f847cba4478379a8
bingo009 <anna.tulasiram@gmail.com>
2023-07-05 10:44:03 +0530

    Event Handler Implemented

--- src/App.jsx
+++ src/App.jsx
@@ -38,11 +38,17 @@ const  List = () =>{
   );
 }
 const  Search = () =>{
+  
+  const handleChange = (event) => {
+      console.log(event);
+      
+      console.log(event.target.value)
+  }
   return (
         <div>
             <h1>My Hacker Stories</h1>
             <label htmlFor="search">Search: </label>
-            <input id="search" type="text" />
+            <input id="search" type="text" onChange={handleChange} />
         </div>
   )
 }
